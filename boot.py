aa6da5bda07b9ba9d745494cfa9896b9a634916f
valik.svs <27958783+valik-svs@users.noreply.github.com>
2020-03-23 15:14:56 +0300

    first commit

---
+++ boot.py
@@ -0,0 +1,32 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+import uos, machine
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
+import network
+
+
+ssid = 'MiWiFi'
+password = 'Kykyryza.12345'
+
+
+def do_connect():
+
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+do_connect()
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,82 @@
+import time
+import machine
+from umqttsimple import MQTTClient
+from machine import ADC
+from machine import Pin
+from machine import WDT
+wdt = WDT()  # enable it with a timeout of 2s
+
+
+
+analog = ADC(0)     # create ADC object on ADC pin
+
+GPIO5_D1 = Pin(5, Pin.OUT)
+GPIO5_D1.on()
+
+mqtt_server = 'mqtt.by'
+mqtt_port = 1883
+mqtt_user = 'valik'
+mqtt_password = '19o9g18o'
+
+client_id = 'wifiRelay'
+topic_prefix = b'/user/valik/'
+topic_sub = topic_prefix + b'krd/zal/clickRelay'
+topic_pub = topic_prefix + b'krd/zal/status'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+
+# Complete project details at https://RandomNerdTutorials.com
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if topic == topic_sub:
+        if msg == b'0':
+            if status != 1024:
+                print('forced turn off')
+                GPIO5_D1.off()
+                time.sleep(3.5)
+                GPIO5_D1.on()
+            else:
+                print('PC is turned off')
+
+        elif msg == b'1':
+            print('turn on')
+            GPIO5_D1.off()
+            GPIO5_D1.on()
+
+def connect_and_subscribe():
+    client = MQTTClient(client_id=client_id, server=mqtt_server, port=mqtt_port, user=mqtt_user, password=mqtt_password)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(topic_sub)
+    print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+    return client
+
+
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+
+
+try:
+    client = connect_and_subscribe()
+except OSError as e:
+    restart_and_reconnect()
+
+while True:
+    try:
+        client.check_msg()
+
+        status = analog.read()
+        msg = b'OFF' if status== 1024 else b'ON'
+        client.publish(topic_pub, msg)
+        time.sleep(1) #sec
+        wdt.feed() # wathdog
+        print(msg)
+
+    except OSError as e:
+        restart_and_reconnect()
